c04e634e3bade6d1938db77167d60262ee6d4c9b
Tom <tom@lawofgamedesign.com>
2020-05-22 15:09:03 -0400

    Set up basic script architecture for inputs
    Added near-blank game manager, services listing, input manager.

---
+++ RLLS/Assets/Scripts/Architecture/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+   
+    private void Start()
+    {
+        Services.Inputs = new InputManager();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
---
+++ RLLS/Assets/Scripts/Architecture/Services.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class Services
+{
+    private static InputManager inputs;
+    public static InputManager Inputs
+    {
+        get
+        {
+            Debug.Assert(inputs != null, "No input manager.");
+            return inputs;
+        }
+        set { inputs = value; }
+    }
+}
